c338d7a9c57ba865a4934e1a83af21eb155a2056
ilaria-giunti <102374311+ilaria-giunti@users.noreply.github.com>
2023-04-05 17:49:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ loom_embed_url = "https://www.loom.com/embed/1b9ab8c05cec4351859af027a0a3a7b2"
 
 st.markdown('Se hai bisogno di una mano per usare questo tool, guarda il video')
 st.markdown(f'<iframe src="{loom_embed_url}" style="width:100%; height:400px; border:none;" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+video_path='https://www.loom.com/share/521b9bfc03844421b556776dc7a6bac2'
+st.markdown(f'<a href="{video_path}"> Clicca qui per vedere come fare questo export Clienti da Shopify</a>', unsafe_allow_html=True)

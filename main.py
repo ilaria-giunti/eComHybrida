c2cade73592fdc85a2b30246692a6b4efb4891f3
ilaria-giunti <102374311+ilaria-giunti@users.noreply.github.com>
2023-04-05 12:52:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pandas as pd
 
 st.title('Benvenuto!')
 st.write('Scegli dal Menù a sinistra cosa vuoi fare')
+
+loom_embed_url = "https://www.loom.com/embed/1b9ab8c05cec4351859af027a0a3a7b2"
+
+st.markdown('Se hai bisogno di una mano per usare questo tool, guarda il video')
+st.markdown(f'<iframe src="{loom_embed_url}" style="width:100%; height:400px; border:none;" allowfullscreen></iframe>', unsafe_allow_html=True)
